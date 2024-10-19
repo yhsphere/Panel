df516d8e767f63582734bf54fdab878345dd8292
yhsphere <huangsphere@gmail.com>
2024-10-19 23:41:41 +0800

    del www.speedtest.net

--- _worker.js
+++ _worker.js
@@ -5107,7 +5107,7 @@ async function getConfigAddresses(hostName, cleanIPs, enableIPv6) {
   const defaultIPv6 = enableIPv6 ? resolved.ipv6.map((ip) => `[${ip}]`) : [];
   return [
     hostName,
-    "www.speedtest.net",
+    // "www.speedtest.net",
     ...resolved.ipv4,
     ...defaultIPv6,
     ...cleanIPs ? cleanIPs.split(",") : []
